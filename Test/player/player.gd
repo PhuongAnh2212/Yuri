e6f6ebceddf029a38c2856d3f6e55b70d8544af2
Phanh <119726597+PhuongAnh2212@users.noreply.github.com>
2023-10-24 02:51:42 +0700

    Basic Collisons
    Block out the main sections

---
+++ Test/player/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed: int = 300
+@onready var animations = $AnimationPlayer
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection*speed
+	
+func updateAnimation():
+	var direction = "Front"
+	if velocity.x < 0: direction = "Left"
+	elif velocity.x > 0: direction = "Right"
+	elif velocity.y < 0: direction = "Back"
+	
+	animations.play("walk" + direction)
+func _physics_process(delta):
+	handleInput()
+	move_and_slide()
+	updateAnimation()
