3a9e3fb1ffaa6631893842cbdb5ff3f5ccb04486
Lê Thị Hà Chi <amuchan134@gmail.com>
2023-12-08 17:12:37 +0700

    add bartender dialogue

--- Test/global.gd
+++ Test/global.gd
@@ -1,4 +1,6 @@
 extends Node
 
 var found_thanhmai = false
-var receive_thanhmai_item = false
+
+var met_bartender = false
+var found_bartender = false
--- Test/test_dialogue.gd
+++ Test/test_dialogue.gd
@@ -1,5 +1,6 @@
 extends BaseDialogueTestScene
 
+
 func _ready() -> void:
 	var balloon = load("res://dialogue balloon/balloon.tscn").instantiate()
 	get_tree().current_scene.add_child(balloon)
